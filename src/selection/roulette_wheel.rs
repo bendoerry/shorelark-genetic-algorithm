92f1ebf91656a3afac3e4cfae28aac2970869085
Benjamin Doerry <ben.doerry@gmail.com>
2021-06-15 20:46:54 +0100

    Start work on testing `select`

--- src/selection/roulette_wheel.rs
+++ src/selection/roulette_wheel.rs
@@ -21,3 +21,22 @@ impl SelectionMethod for RouletteWheelSelection {
             .expect("got an empty population")
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use rand::SeedableRng;
+    use rand_chacha::ChaCha8Rng;
+
+    use crate::selection::{roulette_wheel::RouletteWheelSelection, SelectionMethod};
+
+    #[test]
+    fn test_select() {
+        let mut rng = ChaCha8Rng::from_seed(Default::default());
+
+        let population = vec![ /* what here? */];
+
+        let actual = RouletteWheelSelection::new().select(&mut rng, &population);
+
+        assert!(/* what here? */)
+    }
+}
