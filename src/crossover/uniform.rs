f0d31155741ee2c1e5f9d5c8a3ab43f4c4de564e
Benjamin Doerry <ben.doerry@gmail.com>
2021-06-15 22:47:33 +0100

    Test by counting differences between parent and child

--- src/crossover/uniform.rs
+++ src/crossover/uniform.rs
@@ -43,11 +43,27 @@ mod tests {
     #[test]
     fn test() {
         let mut rng = ChaCha8Rng::from_seed(Default::default());
-        let parent_a = todo!();
-        let parent_b = todo!();
+        let parent_a = (1..=100).map(|n| n as f32).collect();
+        let parent_b = (1..=100).map(|n| -n as f32).collect();
+
+        // First parent will be:
+        //   [1, 2, /* ... */, 100]
+        //
+        // Second parent will look similar, but with reversed signs:
+        //   [-1, -2, /* ... */, -100]
+        //
+        // Just like in the histogram, the concrete number of genes doesn't
+        // matter - 100 will nicely round up to 100%, that's all
 
         let child = UniformCrossover::new().crossover(&mut rng, &parent_a, &parent_b);
 
-        assert!(/* ... */);
+        // Number of genes different between `child` and `parent_a`
+        let diff_a = child.iter().zip(parent_a).filter(|(c, p)| *c != p).count();
+
+        // Number of genes different between `child` and `parent_b`
+        let diff_b = child.iter().zip(parent_b).filter(|(c, p)| *c != p).count();
+
+        assert_eq!(diff_a, 0);
+        assert_eq!(diff_b, 0);
     }
 }
