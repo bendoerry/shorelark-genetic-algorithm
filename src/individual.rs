5fb54cd52641541fc52eb6b0c2de3b290f0e5d14
Benjamin Doerry <ben.doerry@gmail.com>
2021-06-15 22:21:23 +0100

    Add `chromosome` fn to `Individual` trait
    This also implements the function for `TestIndividual`,
    but this implementation will only panic.

--- src/individual.rs
+++ src/individual.rs
@@ -1,4 +1,7 @@
+use crate::chromosome::Chromosome;
+
 pub trait Individual {
+    fn chromosome(&self) -> &Chromosome;
     fn fitness(&self) -> f32;
 }
 
@@ -17,6 +20,10 @@ impl TestIndividual {
 
 #[cfg(test)]
 impl Individual for TestIndividual {
+    fn chromosome(&self) -> &Chromosome {
+        panic!("not supported for TestIndividual")
+    }
+
     fn fitness(&self) -> f32 {
         self.fitness
     }
